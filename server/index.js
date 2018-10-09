286b5d7e7edeff4271e239674a48d7f7ca9c9eb5
9oelM <hj923@hotmail.com>
2018-10-09 08:03:46 +0000

    prettier files

--- server/index.js
+++ server/index.js
@@ -1,22 +1,22 @@
-const proxy = require('http-proxy-middleware')
-const Bundler = require('parcel-bundler')
-const express = require('express')
+const proxy = require("http-proxy-middleware")
+const Bundler = require("parcel-bundler")
+const express = require("express")
 
-let bundler = new Bundler('client/index.html')
+let bundler = new Bundler("client/index.html")
 
 let app = express()
 
 app.use(
-  '/api',
+  "/api",
   proxy({
-    target: 'http://super-fullstack-j031.c9users.io:8081', // target host
-    changeOrigin: true,               // needed for virtual hosted sites
-})
+    target: "http://super-fullstack-j031.c9users.io:8081", // target host
+    changeOrigin: true, // needed for virtual hosted sites
+  })
 )
 app.use(bundler.middleware())
 
-app.get('/api', function(req, res){
-  res.send('<html><body><h1>Hello World</h1></body></html>');
-});
+app.get("/api", function(req, res) {
+  res.send("<html><body><h1>Hello World</h1></body></html>")
+})
 
-app.listen(8081,  () => console.log(`Listening`))
+app.listen(8081, () => console.log(`Listening`))
