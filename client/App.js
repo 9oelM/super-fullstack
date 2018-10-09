7d20b2f182e0b6c74eeb4bad6043a2fdc1468271
9oelM <hj923@hotmail.com>
2018-10-09 07:49:36 +0000

    prettier

--- client/App.js
+++ client/App.js
@@ -2,8 +2,10 @@ import React from "react"
 import "./App.sass"
 import "normalize.css"
 
-const App = () => 
-    <div id = "hello-container"><h1 id = "hello">Helloooooooooooooo worlddddddd</h1></div>
-
+const App = () => (
+  <div id="hello-container">
+    <h1 id="hello">Helloooooooooooooo worlddddddd</h1>
+  </div>
+)
 
 export default App
