f4780366339c75c023c8c5b38158bd643b7c16e8
9oelM <hj923@hotmail.com>
2018-10-10 11:57:13 +0000

    add nodemon, refactor, environment variable settings

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,21 +1,25 @@
-const gulp = require('gulp');
-const run = require('gulp-run-command').default;
-const prettier = require('gulp-prettier');
-const eslint = require('gulp-eslint');
-const prettierrc = require('./.prettierrc');
-const eslintrc = require('./.eslintrc');
+const gulp = require('gulp'),
+      run = require('gulp-run-command').default,
+      prettier = require('gulp-prettier'),
+      eslint = require('gulp-eslint'),
+      nodemon = require('gulp-nodemon'),
+      prettierrc = require('./.prettierrc'),
+      eslintrc = require('./.eslintrc')
 /*
     customize your paths to get it working.
 */
 const paths = {
   js: ['./client/**/*.js','./server/**/*.js']
 }
+const ports = {
+  FRONT_PORT:  process.env.FRONT_PORT || '8080',
+  BACK_PORT: process.env.BACK_PORT || '8081' 
+}
 
 const format = () => gulp
     .src(paths.js, { base: '.', since: gulp.lastRun(format) })
     .pipe(prettier(prettierrc))
     .pipe(gulp.dest('.'))
-
 
 const lint = () => gulp
     .src(paths.js, { base: '.', since: gulp.lastRun(lint) })
@@ -29,12 +33,26 @@ const lint = () => gulp
   // lint error, return the stream and pipe to failAfterError last.
   //  .pipe(eslint.failAfterError()) --> commented out because it can cause error to the next gulp task that receives error code
 
-const dev = run("npm run dev")
+const frontDev = run(`parcel -p ${ports.FRONT_PORT} client/index.html`)
 
-const build = run("npm run build")
-/*
-    generic functions
-*/
+const backDev = () => {
+  nodemon({
+    script: './server/index.js',
+    ext: 'js',
+    env: { 
+    NODE_ENV: 'development',
+    BACK_PORT: ports.BACK_PORT
+    }
+  })
+  .on('start', ()=> {
+    console.log(`nodemon started`)
+  })
+  .on('crash',()=>{
+    console.log(`nodemon crashed`)
+  })
+}
+
+const build = run(`NODE_ENV=production parcel -p ${ports.FRONT_PORT} build client/index.html --out-dir build`)
 
 const watch = () => {
   const watchers = [gulp.watch(paths.js, format), gulp.watch(paths.js, lint)]
@@ -43,10 +61,13 @@ const watch = () => {
   }))
 }
 
+
 /*
     tasks
 */
 exports.watch = watch
-exports.dev = dev
+exports.frontDev = frontDev
+exports.backDev = backDev
+exports.dev = gulp.parallel(frontDev, backDev)
 exports.build = gulp.series(format, lint, build)
-exports.default = gulp.parallel(watch)
+exports.default = gulp.parallel(frontDev)
